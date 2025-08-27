0fb3ac5355fe78c0343999fddcd5805922841dff
Cob <jcobsntos@gmail.com>
2025-08-27 19:08:45 +0800

    fix for render backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 //generate JWT token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: "1hr",
+        expiresIn: "1h",
     });
 }
 
